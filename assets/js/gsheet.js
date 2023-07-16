7d8209ff5d6faa3f051fcbceb874907ac0017245
areenasjayan333 <areesjayan123@gmail.com>
2023-07-16 18:51:51 +0530

    contact form linked with gsheet for testing2

--- assets/js/gsheet.js
+++ assets/js/gsheet.js
@@ -1,11 +1,14 @@
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyNMWCqylD3dgsWubI2AoU1Uv1GUnr64N3bz7uY55GzdMhr8tdQ8QdK1nyVQgwL0JU/exec'
-const form = document.forms['contact-form']
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbyNMWCqylD3dgsWubI2AoU1Uv1GUnr64N3bz7uY55GzdMhr8tdQ8QdK1nyVQgwL0JU/exec'
+// const form = document.forms['contact-form']
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
-  .then(() => {  window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-})
+// form.addEventListener('submit', e => {
+//   e.preventDefault()
+//   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//   .then(response => alert("Thank you! your form is submitted successfully." ))
+//   .then(() => {  window.location.reload(); })
+//   .catch(error => console.error('Error!', error.message))
+// })
+
+
+
